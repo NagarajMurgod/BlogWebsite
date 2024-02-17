4e04ef2251af69aca37da8e9e15083719ce972f8
IAM5KID <01fe19bee418@gmail.com>
2024-02-18 03:13:35 +0530

    nave bar creation in progress

---
+++ MyBlog/static/common/js/navbar.js
@@ -0,0 +1,15 @@
+
+var cbtn = document.getElementById('category-btn');
+var cat_itms = document.querySelector('.category-items')
+console.log(cat_itms)
+
+cbtn.addEventListener('click', function(){
+    var disp = cat_itms.style.display;
+    if(disp === 'block'){
+        cat_itms.style.display = 'none';
+    }
+    else{
+        cat_itms.style.display = 'block'
+    }
+    console.log('wroking');
+})
